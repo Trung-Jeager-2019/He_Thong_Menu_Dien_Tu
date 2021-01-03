fb501ff67ee8cfe617fe88c3095737a495e93d9c
Trung-Jeager-2019 <Trung-Jeager-2019@github.com>
2021-01-04 00:50:54 +0700

    setup static az

--- Menu_Dien_Tu/settings.py
+++ Menu_Dien_Tu/settings.py
@@ -178,6 +178,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets').replace('\\', '/') 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
